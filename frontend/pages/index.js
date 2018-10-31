99161be022ec9d460c0d93e3f5a64134dc6e3678
ItsY8S <gri.y8s@gmail.com>
2018-10-31 19:05:47 -0400

    Fixed folder structure.

---
+++ frontend/pages/index.js
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+
+const Dashboard = props => {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+    </div>
+  )
+}
+
+export default Dashboard
---
+++ frontend/pages/products.js
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+
+const Products = props => {
+  return (
+    <div>
+      <h1>Products</h1>
+    </div>
+  )
+}
+
+export default Products
