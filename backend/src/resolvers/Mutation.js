6ff0aacd1f5838c9a8ebea9a4344d59342db4d02
ItsY8S <gri.y8s@gmail.com>
2018-11-14 18:21:30 -0500

    Modified the schema and also added a resolver to handle user signup.

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,6 @@
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
 const Mutations = {
   async createProduct(parent, args, ctx, info) {
     const product = await ctx.db.mutation.createProduct(
@@ -28,6 +31,27 @@ const Mutations = {
     const where = { id: args.id }
     const item = await ctx.db.query.product({ where }, `{ id title }`)
     return ctx.db.mutation.deleteProduct({ where }, info)
+  },
+  async signup(parent, args, ctx, info) {
+    args.email = args.email.toLowerCase()
+    const password = await bcrypt.hash(args.password, 10)
+    const user = await ctx.db.mutation.createUser(
+      {
+        data: {
+          ...args,
+          password,
+          permissions: { set: ['USER'] }
+        }
+      },
+      info
+    )
+
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET)
+    ctx.response.cookie('token', token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365 // 1 year
+    })
+    return user
   }
 }
 
