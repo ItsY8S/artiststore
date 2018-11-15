3d90fdca2d6cfba533cb342bfca46e0911d5a28a
ItsY8S <gri.y8s@gmail.com>
2018-11-14 22:59:41 -0500

    Users can now sign up.

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -3,7 +3,18 @@ const { forwardTo } = require('prisma-binding')
 const Query = {
   products: forwardTo('db'),
   product: forwardTo('db'),
-  productsConnection: forwardTo('db')
+  productsConnection: forwardTo('db'),
+  me(parent, args, ctx, info) {
+    if (!ctx.request.userId) {
+      return null
+    }
+    return ctx.db.query.user(
+      {
+        where: { id: ctx.request.userId }
+      },
+      info
+    )
+  }
 }
 
 module.exports = Query
